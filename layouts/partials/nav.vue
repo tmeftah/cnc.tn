37fd8a4ec8b21c96dfd3cba7468087000664eed8
Tarek Meftah <sudtechnics@gmail.com>
2019-10-19 18:45:24 +0200

    fix navbar hight on small divices

--- layouts/partials/nav.vue
+++ layouts/partials/nav.vue
@@ -1,9 +1,11 @@
 <template>
   <b-navbar
+    :class="scroll"
     toggleable="lg"
     type="light"
     variant="white"
     sticky
+    @scroll.native="handleScroll"
   >
     <b-navbar-brand
       to="/"
@@ -44,8 +46,66 @@
     </b-collapse>
   </b-navbar>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      scroll: ''
+    }
+  },
+
+  mounted () {
+    document.addEventListener('scroll', this.handleScroll)
+  },
+  destroyed () {
+    document.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll (event) {
+      this.pos = window.scrollY
+      if (window.scrollY > 150) {
+        this.scroll = 'scroll'
+      } else if (window.scrollY < 25) {
+        this.scroll = ''
+      }
+    }
+  }
+}
+</script>
 <style  >
 .navbar {
-  border-bottom: 1px solid #d8d8d8;
+  -webkit-transition: height 0.3s ease-in-out;
+  -moz-transition: height 0.3s ease-in-out;
+  -o-transition: height 0.3s ease-in-out;
+  -ms-transition: height 0.3s ease-in-out;
+  transition: height 0.3s ease-in-out;
+}
+
+.nav-link.nuxt-link-active {
+  border-bottom: 2px solid #0f3c6c;
+  margin-right: 1em;
+}
+.nav-link {
+  border-bottom: 2px solid #b1b2a933;
+  margin-right: 1em;
+  padding-right: 0px !important;
+  padding-left: 0px !important;
+}
+@media (min-width: 992px) {
+  .navbar.scroll {
+    border-bottom: 1px solid #d8d8d8;
+    -webkit-transition: height 0.3s ease-in-out;
+    -moz-transition: height 0.3s ease-in-out;
+    -o-transition: height 0.3s ease-in-out;
+    -ms-transition: height 0.3s ease-in-out;
+    transition: height 0.3s ease-in-out;
+
+    height: 80px;
+  }
+}
+@media (max-width: 992px) {
+  .navbar.scroll {
+    border-bottom: 1px solid #d8d8d8;
+  }
 }
 </style>
