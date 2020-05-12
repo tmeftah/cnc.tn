65888de11fcde2afa0edc00b55a2d04f1ff3f074
tmeftah <sudtechnics@gmail.com>
2020-05-12 02:58:44 +0200

    add material-design-icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Poppins'
+        href: 'https://fonts.googleapis.com/css?family=Montserrat'
       }
     ]
   },
@@ -84,7 +84,8 @@ export default {
         defaultLocale: 'fr',
         lazy: true
       }
-    ]
+    ],
+    'nuxt-material-design-icons'
   ],
   /*
    ** Axios module configuration
