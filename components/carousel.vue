8a6c293fba7b46715b446919200690459b82357f
tmeftah <sudtechnics@gmail.com>
2020-05-14 03:50:47 +0200

    reḿove youtube videos embed

--- components/carousel.vue
+++ components/carousel.vue
@@ -11,7 +11,7 @@
             height="50"
           />
         </div>
-        <p class="h4">{{$t('fabrication_100')}}</p>
+        <p class="h4">{{ $t('fabrication_100') }}</p>
       </b-col>
     </b-row>
     <b-row>
@@ -41,67 +41,60 @@
     <b-row>
       <b-col cols="12" class="mb-4">
         <p class="h2">CNC PREMIUM ST en vidéos</p>
-        <p
-          class="h4"
-        >Consulter les vidéos mises sur notre site ou regarder les toutes sur notre chaîne youtube</p>
+        <p class="h4">
+          Consulter les vidéos mises sur notre site ou regarder les toutes sur
+          notre chaîne youtube
+        </p>
       </b-col>
       <b-col cols="12" sm="3">
-        <div class="embed-responsive embed-responsive-16by9 mb-2">
-          <iframe
-            class="embed-responsive-item"
-            src="https://www.youtube.com/embed/Kps3tn_g2KA"
-            allowfullscreen
-          />
-        </div>
+        <img
+          width="100%"
+          src="https://img.youtube.com/vi/Kps3tn_g2KA/maxresdefault.jpg"
+          alt="video-youtube-cnc-malek-meftah"
+        />
       </b-col>
 
       <b-col cols="12" sm="3">
-        <div class="embed-responsive embed-responsive-16by9 mb-2">
-          <iframe
-            class="embed-responsive-item"
-            src="https://www.youtube.com/embed/lGPZTKcCoEA"
-            allowfullscreen
-          />
-        </div>
+        <img
+          width="100%"
+          src="https://img.youtube.com/vi/lGPZTKcCoEA/maxresdefault.jpg"
+          alt="video-youtube-cnc-malek-meftah"
+        />
       </b-col>
       <b-col cols="12" sm="3">
-        <div class="embed-responsive embed-responsive-16by9 mb-2">
-          <iframe
-            class="embed-responsive-item"
-            src="https://www.youtube.com/embed/6lMv8f-PvFA"
-            allowfullscreen
-          />
-        </div>
+        <img
+          width="100%"
+          src="https://img.youtube.com/vi/6lMv8f-PvFA/maxresdefault.jpg"
+          alt="video-youtube-cnc-malek-meftah"
+        />
       </b-col>
       <b-col cols="12" sm="3">
-        <div class="embed-responsive embed-responsive-16by9">
-          <iframe
-            class="embed-responsive-item"
-            src="https://www.youtube.com/embed/58nY_BRYTnE"
-            allowfullscreen
-          />
-        </div>
+        <img
+          width="100%"
+          src="https://img.youtube.com/vi/Srk1U5s_CFA/maxresdefault.jpg"
+          alt="video-youtube-cnc-malek-meftah"
+        />
       </b-col>
     </b-row>
   </div>
 </template>
 
 <script>
-  export default {}
+export default {}
 </script>
 
-<style >
-  .bordertop {
-    border-top: 1px solid #d8d8d8;
-    -webkit-box-shadow: 0px 0px 54px -24px #000000bf;
-    -moz-box-shadow: 0px 0px 54px -24px #000000bf;
-    box-shadow: 0px 0px 54px -24px #000000bf;
-    padding-top: 2em;
-  }
-  .carousel-indicators li {
-    background-color: #2d4764;
-  }
-  .carousel-indicators .active {
-    background-color: #0f3c6c;
-  }
+<style>
+.bordertop {
+  border-top: 1px solid #d8d8d8;
+  -webkit-box-shadow: 0px 0px 54px -24px #000000bf;
+  -moz-box-shadow: 0px 0px 54px -24px #000000bf;
+  box-shadow: 0px 0px 54px -24px #000000bf;
+  padding-top: 2em;
+}
+.carousel-indicators li {
+  background-color: #2d4764;
+}
+.carousel-indicators .active {
+  background-color: #0f3c6c;
+}
 </style>
